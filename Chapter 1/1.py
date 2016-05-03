9ec05f712f0e0b4284ff163ba026b99b5380fdbd
nieoh <steph@nieoh.com>
2016-05-02 22:39:49 -0700

    added two more implementations using hash tables and sets respectively.

--- Chapter 1/1.py
+++ Chapter 1/1.py
@@ -7,9 +7,40 @@ def uniqueQ(word):
 			return False
 	return True
 
-print "Some tests. Expected: False, True, True, False"
+#run time O(nlogn)
+print "Some tests with no extra ds. Expected: False, True, True, False"
 print uniqueQ('hello')
 print uniqueQ("asdf")
 print uniqueQ("12kf9e d")
 print uniqueQ("   ")
+
+
+def uniqueQWithHashBrowns(word):
+	letters = dict()
+	for c in word:
+		if c in letters:
+			return False
+		else:
+			letters[c] = "potato"
+	return True
+
+#run time O(n)
+print "Some tests with hash table. Expected: False, True, True, False"
+print uniqueQWithHashBrowns('hello')
+print uniqueQWithHashBrowns("asdf")
+print uniqueQWithHashBrowns("12kf9e d")
+print uniqueQWithHashBrowns("   ")
+
+
+def uniqueQWithSet(word):
+	s = set(word)
+	return len(s) == len(word)
+
+#run time O(n)
+print "Some tests with set. Expected: False, True, True, False"
+print uniqueQWithSet('hello')
+print uniqueQWithSet("asdf")
+print uniqueQWithSet("12kf9e d")
+print uniqueQWithSet("   ")
+
 
